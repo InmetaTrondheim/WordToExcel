d299b1d425a0742241445ab12a0a15d650a10c88
TordStavik <tord.stavik@inmeta.no>
2019-02-21 16:30:48 +0100

    Check if paragraph 1 or 3 or breadtext has anything in them and print to column c in excel

--- WordToExcelWPF/MainWindow.xaml.cs
+++ WordToExcelWPF/MainWindow.xaml.cs
@@ -21,7 +21,8 @@ namespace WordToExcelWPF
     public partial class MainWindow : System.Windows.Window
     {
         List<string> heading4List = new List<string>();
-        string[] numbersArr= new string[9999];
+        List<string> errorList = new List<string>();
+        string[] numbersArr = new string[9999];
         int totHours;
         string source;
         string destinationFolder;
@@ -35,11 +36,11 @@ namespace WordToExcelWPF
         {
             System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
             openFileDialog.ShowDialog();
-            
-                source = openFileDialog.FileName;
-                txbInput.Text = source;
-                
-            
+
+            source = openFileDialog.FileName;
+            txbInput.Text = source;
+
+
         }
 
         private void BtnDestinationFolder_Click(object sender, RoutedEventArgs e)
@@ -51,7 +52,7 @@ namespace WordToExcelWPF
 
 
 
-        private  void ReadWord()
+        private void ReadWord()
         {
             Microsoft.Office.Interop.Word.Application ap = new Microsoft.Office.Interop.Word.Application();
             Document document = ap.Documents.Open(txbInput.Text);
@@ -60,7 +61,6 @@ namespace WordToExcelWPF
                 Microsoft.Office.Interop.Word.Style style = paragraph.get_Style() as Microsoft.Office.Interop.Word.Style;
                 string styleName = style.NameLocal;
                 string text = paragraph.Range.Text;
-
 
                 if (styleName == "Heading 2")
                 {
@@ -90,13 +90,25 @@ namespace WordToExcelWPF
                     arrayCounter++;
 
                 }
+                else if(styleName == "Heading 1")
+                {
+                    AddNumberToList(text, true, "heading 1");
+                }
+                else if(styleName == "Heading 3")
+                {
+                    AddNumberToList(text, true, "heading 3");
+                }
+                else
+                {
+                    AddNumberToList(text, true, "everywhere else");
+                }
 
             }
 
             document.Close();
         }
 
-        private void AddNumberToList(string text)
+        private void AddNumberToList(string text, bool comingFromWrong = false, string fromWhere = "")
         {
             if (text.Contains('('))
             {
@@ -104,9 +116,17 @@ namespace WordToExcelWPF
                 string resultString = Regex.Match(after, @"\d+").Value;
                 if (resultString != "")
                 {
-                    numbersArr[arrayCounter] = resultString;
-                    int hour = int.Parse(resultString);
-                    totHours += hour;
+                    if (comingFromWrong)
+                    {
+                        errorList.Add(fromWhere + " " +text);
+                    }
+                    else
+                    {
+                        numbersArr[arrayCounter] = resultString;
+                        int hour = int.Parse(resultString);
+                        totHours += hour;
+                    }
+     
                 }
             }
         }
@@ -134,29 +154,42 @@ namespace WordToExcelWPF
                 counter = 1;
                 for (int i = 0; i < 999; i++)
                 {
-                    if(numbersArr[i] != null)
+                    if (numbersArr[i] != null)
                     {
                         worksheet.Cells["B" + i].Value = numbersArr[i];
                     }
+                }
+
+                int counterError = 1;
+                foreach (string error in errorList)
+                {
+                    worksheet.Cells["C" + counterError].Value = error;
+                    counterError++;
                 }
 
                 //worksheet.Cells["A" + counter].Value = totHours.ToString();
 
                 FileInfo excelFile = new FileInfo(destinationFolder + @"\" + txbOutput.Text);
                 excel.SaveAs(excelFile);
-                
+
             }
 
         }
 
         private async void BtnConvert_Click_1(object sender, RoutedEventArgs e)
         {
+            heading4List = new List<string>();
+            numbersArr = new string[9999];
+            arrayCounter = 1;
+            totHours = 0;
+            errorList = new List<string>();
+
             lblWorking.Visibility = Visibility.Visible;
 
-           
-                ReadWord();
 
-                CreateExcel();
+            ReadWord();
+
+            CreateExcel();
             lblWorking.Visibility = Visibility.Hidden;
 
             System.Windows.MessageBox.Show("Done");
